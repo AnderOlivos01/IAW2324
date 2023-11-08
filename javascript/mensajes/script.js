bf1d9ee499ed60cd95eb83b33ab90dc29e26cd83
AnderOlivos01 <andersonolivos01@iesamachado.org>
2023-11-08 01:18:46 +0100

    mensajes casi terminado

--- javascript/mensajes/script.js
+++ javascript/mensajes/script.js
@@ -2,16 +2,35 @@ let oldcoments=[];
 haycomenarios(comentarios);
 
 function publicar(a,b){
-    var newcoments='';
-    oldcoments.push(a.value);
-    for(var i=0;i<oldcoments.length;i++){
-         newcoments+="<p id='coment'>"+oldcoments[i]+"</p>";
+    if(a.value!=''){
+        var newcoments='';
+        oldcoments.push(a.value);
+        oldcoments.sort();
+        for(var i=0;i<oldcoments.length;i++){
+             newcoments+="<div class='coment-div'><p class='coment-p'>"+oldcoments[i]+"</p><img class='basura' src='trash.png' onclick='borrar("+i+")'></div>";
+        }
+        b.innerHTML=newcoments;
     }
-    b.innerHTML=newcoments;
 }
 
 function haycomenarios(a){
     if(oldcoments.length==0){
-        a.innerHTML="<p>Aún no hay comentarios</p>"
+        a.innerHTML="<p id='no-coments'>Aún no hay comentarios</p>"
     }
 }
+
+function borrar(a,b){
+    oldcoments.splice(a,1);
+    oldcoments.sort();
+        var newcoments='';
+        for(var i=0;i<oldcoments.length;i++){
+            newcoments+="<div class='coment-div'><p class='coment-p'>"+oldcoments[i]+"</p><img class='basura' src='trash.png' onclick='borrar("+i+")'></div>";
+        }
+        document.getElementById('comentarios').innerHTML=newcoments;
+    }
+
+document.getElementById('mensaje').addEventListener("keydown",(event)=>{
+    if(event.key=="Enter"){
+        publicar(mensaje,comentarios);
+    }
+}, false);
