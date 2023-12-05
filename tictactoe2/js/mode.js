f492a936fac05e4577471e17a31d95121e28e8f7
Anderson Olivos <andersonolivos01@iesamachado.org>
2023-12-05 10:19:51 +0100

    tictactoe repo privado

--- tictactoe2/js/mode.js
+++
@@ -1,7 +0,0 @@
-select_mode();
-function select_mode(){
-    $("#game-name").css({"display":"block"});
-    $("#container-mode").css({"display":"flex"});
-    $("#container-create-user").css({"display":"none"});
-    $("#container-game").css({"display":"none"});
-}
